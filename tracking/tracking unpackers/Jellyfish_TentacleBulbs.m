610307c115e9104b9e0a1fa15e5050f38e32ccdc
Ann Kennedy <annkennedy05@gmail.com>
2017-07-17 15:07:14 -0700

    more formatting tweaks

--- tracking/tracking unpackers/Jellyfish_TentacleBulbs.m
+++ tracking/tracking unpackers/Jellyfish_TentacleBulbs.m
@@ -3,4 +3,4 @@ function pts = Jellyfish_TentacleBulbs(data,fr)
 % npts = size(data.ctrStore{fr},1);
 % pts = [(1:npts)' data.ctrStore{fr}];
 npts = 16;
-pts = [(1:npts)' bsxfun(@plus,squeeze(data.tracked(fr,:,:)),[324 277])];
+pts = [(1:npts)' bsxfun(@plus,squeeze(data.tracked(fr,:,:)),[326 277]) 5*ones(npts,1)];
