20b7c3ddf049efd280f70dba132e95d358971f19
Ann Kennedy <annkennedy05@gmail.com>
2017-07-17 14:52:33 -0700

    parameter tweak

--- tracking/tracking unpackers/Jellyfish_TentacleBulbs.m
+++ tracking/tracking unpackers/Jellyfish_TentacleBulbs.m
@@ -3,4 +3,4 @@ function pts = Jellyfish_TentacleBulbs(data,fr)
 % npts = size(data.ctrStore{fr},1);
 % pts = [(1:npts)' data.ctrStore{fr}];
 npts = 16;
-pts = [(1:npts)' bsxfun(@plus,squeeze(data.tracked(fr,:,:)),[323 277])];
+pts = [(1:npts)' bsxfun(@plus,squeeze(data.tracked(fr,:,:)),[324 277])];
