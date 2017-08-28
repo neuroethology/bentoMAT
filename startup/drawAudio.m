99889be55416b7a82873d3ed825fa765a85d16fd
Ann Kennedy <annkennedy05@gmail.com>
2017-08-27 22:47:57 -0700

    spectrogram scaling

--- startup/drawAudio.m
+++ startup/drawAudio.m
@@ -8,7 +8,7 @@ audio.panel        = uipanel('position',[0 0 1 1],'bordertype','none');
 audio.axes         = axes('parent',audio.panel,'ytick',[]); hold on;
 audio.yScale       = 1;
 audio.win          = 20;
-audio.img          = image();
+audio.img          = image();colormap(parula(64));
 audio.zeroLine     = plot([0 0],get(gca,'ylim'),'k--');
 
 xlabel('time (sec)');
