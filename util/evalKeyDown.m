3e0210da25e03dcfa15ab239ee64321bb8071595
Ann Kennedy <annkennedy05@gmail.com>
2017-12-11 12:07:40 -0800

    keyboard support for annotation

--- util/evalKeyDown.m
+++ util/evalKeyDown.m
@@ -2,12 +2,22 @@ function evalKeyDown(source,eventdata)
 
 gui=guidata(source);
 
-switch eventdata.Key 
+switch eventdata.Key
+    case 'pagedown'
+        if(any(gui.Action~=[gui.ctrl.slider.SliderStep(2) 0]))
+            gui.ctrl.slider.timer = tic;
+        end
+        gui.Action = [gui.ctrl.slider.SliderStep(2) 0];
     case 'rightarrow'
         if(any(gui.Action~=[gui.ctrl.slider.SliderStep(1) 0]))
             gui.ctrl.slider.timer = tic;
         end
         gui.Action = [gui.ctrl.slider.SliderStep(1) 0];
+    case 'pageup'
+        if(any(gui.Action~=[-gui.ctrl.slider.SliderStep(2) 0]))
+            gui.ctrl.slider.timer = tic;
+        end
+        gui.Action = [-gui.ctrl.slider.SliderStep(2) 0];
     case 'leftarrow'
         if(any(gui.Action~=[-gui.ctrl.slider.SliderStep(1) 0]))
             gui.ctrl.slider.timer = tic;
--- util/evalKeyUp.m
+++ util/evalKeyUp.m
@@ -1,10 +1,6 @@
 function evalKeyUp(source,eventdata)
 
 gui=guidata(source);
-% if(isempty(gui.annot.activeCh))
-%     return
-% end
-
 switch eventdata.Key
     case 'space'
         if(~gui.Action)
@@ -13,8 +9,30 @@ switch eventdata.Key
         else
             clearAction(source);
         end
+    case 'pagedown'
+        clearAction(source);
+    case 'pageup'
+        clearAction(source);
     case 'rightarrow'
         clearAction(source);
     case 'leftarrow'
         clearAction(source);
+    case 'a'
+        if(~isempty(gui.annot.activeCh))
+            gui.ctrl.annot.toggleAnnot.Value = ~gui.ctrl.annot.toggleAnnot.Value;
+            toggleAnnot(gui.ctrl.annot.toggleAnnot);
+        end
+    case 's'
+        if(~isempty(gui.annot.activeCh))
+            % save annotations!
+        end
+    case 'e'
+        if(~isempty(gui.annot.activeCh))
+            gui.ctrl.annot.toggleErase.Value = ~gui.ctrl.annot.toggleErase.Value;
+            toggleAnnot(gui.ctrl.annot.toggleErase);
+        end
+    case 't'
+        if(~isempty(gui.annot.activeCh))
+            quickSave(gui.ctrl.annot.save);
+        end
 end
