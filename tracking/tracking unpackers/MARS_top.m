f9c60a94f2b89a6285458c783c98fe640afd54cd
Ann Kennedy <annkennedy05@gmail.com>
2017-12-12 09:52:54 -0800

    json support

--- tracking/tracking unpackers/MARS_top.m
+++ tracking/tracking unpackers/MARS_top.m
@@ -1,7 +1,7 @@
 function pts = MARS_top(data,fr)
 
 inds    = [1 2 4 5 7 6 4 3 1];
-if(iscell(data.keypoints))
+if(iscell(data.keypoints)) %support for both jsondecode and loadjson
     v       = data.keypoints{fr};
     v       = permute(reshape(v,[2 2 7]),[2 1 3]);
 else
@@ -11,8 +11,3 @@ m1      = squeeze(v(1,:,inds));
 m2      = squeeze(v(2,:,inds));
 
 pts = [1 m1(:)'; 2 m2(:)'];
-
-
-
-
-
