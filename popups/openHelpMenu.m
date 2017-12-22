ea968c8985205195358dfb191901ee7500a04329
Ann Kennedy <annkennedy05@gmail.com>
2017-12-22 13:43:45 -0800

    More hotkey support

--- popups/openHelpMenu.m
+++ popups/openHelpMenu.m
@@ -1,13 +1,13 @@
 function openHelpMenu(~,~)
 
-str = {'Playback Hotkeys---',''};
+str = {'---Playback Hotkeys---',''};
 str = [str;{[char(8592) ' ' char(8594)],'Step +/- one frame'}];
 str = [str; {'PgUp PgDown','Step +/- one second'}];
-str = [str; {'[Spacebar]','Start/stop movie'}];
+str = [str; {'[Spacebar]','Start/stop playback'}];
 str = [str; {[char(8593) ' ' char(8595)],'inc/dec play speed'}];
 str = [str; {'',''}];
 
-str = [str; {'Navigating annotations---',''}];
+str = [str; {'---Navigating annotations---',''}];
 str = [str; {'t/g','Cycle current behavior'}];
 str = [str; {'f','Next bout (any bhvr)'}];
 str = [str; {'b','Prev bout (any bhvr)'}];
@@ -15,11 +15,16 @@ str = [str; {'[Shift]+f','Next bout (current bhvr)'}];
 str = [str; {'[Shift]+b','Prev bout (current bhvr)'}];
 str = [str; {'',''}];
 
-str = [str; {'Modifying annotations---',''}];
+str = [str; {'---Modifying annotations---',''}];
 str = [str; {'a','Toggle annot (current bhvr)'}];
 str = [str; {'e','Toggle erase (current bhvr)'}];
 str = [str; {'[Shift]+e','Toggle erase (all bhvrs)'}];
 str = [str; {'[Ctrl]+z','Undo'}];
+str = [str; {'s','Save changes to annotations'}];
+str = [str; {'',''}];
+str = [str; {'---Other---',''}];
+str = [str; {'h','Open this menu'}];
+str = [str; {'',''}];
 
 str(:,1) = pad(str(:,1),10,'both');
 str = pad(str,15);
