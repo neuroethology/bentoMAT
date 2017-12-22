ae707217b2ffcae7721e343cad28e7f48961acbe
Ann Kennedy <annkennedy05@gmail.com>
2017-12-21 20:50:20 -0800

    loading data

--- data_builder/loadIOData.m
+++ data_builder/loadIOData.m
@@ -2,3 +2,17 @@ function [data,enabled] =loadIOData(io,prev)
 % load and organize files associated with a data entry
 
 keyboard
+data = io;
+for f = fieldnames(io)'
+    if(~(exist(io.(f{:}))==2)) %it's a piece of metadata
+        if(~isempty(str2num(data.(f{:}))))
+            data.(f{:}) = str2num(data.(f{:}));
+        end
+    else %it's a data file to be loaded
+        if(~isempty(prev) && isfield(pref,f{:}) && strcmpi(data.(f{:}),prev.(f{:}))) %file is same as in prev
+            
+        else
+            [pth,fname,ext] = fileparts(io.(f{:}));
+        end
+    end
+end
