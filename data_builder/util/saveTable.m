2902013976ebdbe915bbd2d45fc71d216640f643
Ann Kennedy <annkennedy05@gmail.com>
2018-02-12 11:28:14 -0800

    linux xls file support

--- data_builder/util/saveTable.m
+++ data_builder/util/saveTable.m
@@ -9,5 +9,19 @@ if(FileName)
     if(~isempty(dir([PathName FileName])))
         delete([PathName FileName]); %clear out old data (the lazy way!)
     end
-    xlswrite([PathName FileName],M,'Sheet1');
+    if(exist('xlwrite',2))
+        try
+        xlwrite([PathName FileName],M); % for linux computers
+        catch
+            disp('Save failed. Make sure you have Apache POI 3.8 or 3.9 on your Matlab Java path:');
+            disp('javaaddpath(''poi-3.9/ooxml-lib/dom4j-1.6.1.jar'');');
+            disp('javaaddpath(''poi-3.9/ooxml-lib/xmlbeans-2.3.0.jar'');');
+            disp('javaaddpath(''poi-3.9/poi-3.9-20121203.jar'');');
+            disp('javaaddpath(''poi-3.9/poi-ooxml-3.9-20121203.jar'');');
+            disp('javaaddpath(''poi-3.9/poi-ooxml-schemas-3.9-20121203.jar'');');
+            disp('(replace ''poi-3.9'' with the full path to your poi library.)');
+        end
+    else
+        xlswrite([PathName FileName],M,'Sheet1');
+    end
 end
