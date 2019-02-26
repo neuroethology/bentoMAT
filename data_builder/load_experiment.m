63dc2b2866f379745f8d3ae1d92ef6a572953c85
Ann Kennedy <kennedya@caltech.edu>
2019-02-25 16:15:12 -0800

    handling relative paths in excel sheets

--- data_builder/load_experiment.m
+++ data_builder/load_experiment.m
@@ -3,10 +3,12 @@ function mouse = load_experiment(filename,skipvideos)
 if(isempty(filename))
     [filename pth] = uigetfile('*.xls;*.xlsx');
     filename = [pth filename];
+else
+    pth = fileparts(filename);
 end
 
 [~,~,raw] = xlsread(filename,'Sheet1');
-if(isempty(raw{1,1}))
+if(isempty(raw{1,1})|isnan(raw{1,1}))
     raw{1,1} = pth; %blank path means read from the directory the sheet is in
 end
 if(~exist('skipvideos','var'))
