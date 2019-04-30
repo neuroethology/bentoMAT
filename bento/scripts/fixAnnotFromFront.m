67ff55f13b4ce688fda562fa77bbdad9d61cc4dd
Ann Kennedy <kennedya@caltech.edu>
2019-04-30 13:24:02 -0700

    better annot fixing

--- bento/scripts/fixAnnotFromFront.m
+++ bento/scripts/fixAnnotFromFront.m
@@ -17,8 +17,10 @@ pth(end) = strrep(pth(end),'\','')
 files = dir([pth '\**\*.annot']);
 drop = find(~cellfun(@isempty,strfind({files.name},'_TS.annot')));
 files(drop)=[];
+files2 = dir([pth '**\*.txt']);
+files = [files;files2];
 
-ans = questdlg(['Found ' num2str(length(files)) ' annot files. Continue?'],'Converting frames to times','Yes','No','Yes');
+ans = questdlg(['Found ' num2str(length(files)) ' files. Continue?'],'Converting frames to times','Yes','No','Yes');
 if(strcmpi(ans,'No'))
     return;
 end
@@ -26,40 +28,56 @@ end
 for f = 1:length(files)
     disp([num2str(f) '/' num2str(length(files))]);
     fname = [files(f).folder filesep files(f).name];
+    [~,~,ext] = fileparts(fname);
 
-% get the movie and stim names
-    fid     = fopen(fname);
-    tline   = fgetl(fid);
-    clear movieNames stim;
-    for i=1:2
-        movieNames = tline;
-        tline = fgetl(fid);
-    end
-    for i=1:2
-        stim = tline;
-        tline = fgetl(fid);
-    end
-    fclose(fid);
-    stim = strrep(stim,'Stimulus name: ','');
-    mov = strsplit(strrep(movieNames,'Movie file(s):  ',''),'.seq');
-    mov = [mov{find(~cellfun(@isempty,strfind(mov,'Front')))} '.seq'];
-
-    if(isempty(strrep(mov,'.seq','')))
-        disp(['No movie info for ' fname]);
-        continue;
-    end
-    if(~exist(mov,'file'))
-        [~,mov2] = fileparts(mov);
-        mov2 = strrep(mov2,'Mouse_','');
-        mov2 = dir([fileparts(files(f).folder) filesep '*' mov2 '*.seq']);
-        if(doubleStepBack)
-            mov2 = dir([fileparts(fileparts(files(f).folder)) filesep '**/*' mov2 '*.seq']);
-        end
-        if(isempty(mov2))
-            disp([mov ' could not be found!']);
+    if(strcmpi(ext,'.txt'))
+        mov = regexprep(fname,'_[a-z_]*actions_pred_v[0-9]*_[0-9]*.txt','_Front.seq');
+        [pth,mov] = fileparts(mov);
+        pth = fileparts(fileparts(pth));
+        mov = [pth filesep mov];
+        if(~exist(mov,'file'))
+            disp('Couldn''t find movie! Looked in:');
+            disp(['    ' mov]);
             continue;
-        else
-            mov = [mov2(1).folder filesep mov2(1).name];
+        end
+    else
+        % first try getting the movie name from the annot file:
+        fid     = fopen(fname);
+        tline   = fgetl(fid);
+        clear movieNames stim;
+        for i=1:2
+            movieNames = tline;
+            tline = fgetl(fid);
+        end
+        for i=1:2
+            stim = tline;
+            tline = fgetl(fid);
+        end
+        fclose(fid);
+        stim = strrep(stim,'Stimulus name: ','');
+        mov = strsplit(strrep(movieNames,'Movie file(s):  ',''),'.seq');
+        mov = [mov{find(~cellfun(@isempty,strfind(mov,'Front')))} '.seq'];
+
+        % if that fails, look for a movie with a compatible name
+        if(isempty(strrep(mov,'.seq','')) || ~exist(mov,'file'))
+            if(isempty(strrep(mov,'.seq','')))
+                disp(['No movie info provided in ' fname '; searching based on filename...']);
+            end
+            [~,mov2] = fileparts(mov);
+            mov2 = strrep(mov2,'Mouse_','');
+            mov2 = regexprep(mov2,'_[a-z_]*actions_pred_v[0-9]*_[0-9]*','');
+
+            mov2 = dir([fileparts(files(f).folder) filesep '*' mov2 '*.seq']);
+            if(doubleStepBack)
+                mov2 = dir([fileparts(fileparts(files(f).folder)) filesep '**/*' mov2 '*.seq']);
+            end
+            if(isempty(mov2))
+                disp([mov ' could not be found!']);
+                continue;
+            else
+                disp(['found a match ' mov2(1).name]);
+                mov = [mov2(1).folder filesep mov2(1).name];
+            end
         end
     end
 
